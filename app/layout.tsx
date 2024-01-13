0a37fb2e4930ee3b863c5d2300ea9bfb1c91c45e
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2024-01-14 01:17:38 +0530

    fix: root layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
 import './global.scss';
 
 export { default } from '@/root.layout';
+
+export const metadata = {
+  title: 'Estore',
+  description: 'Ecommerce store built with Next.js and TailwindCSS',
+  metadataBase: new URL('https://estore-lovat.vercel.app'),
+  openGraph: {
+    title: 'Estore',
+    description: 'Ecommerce store built with Next.js and TailwindCSS',
+  },
+};
