c35f85a241eb4dee185fd5af6ea7c1bf53ac15f9
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2024-01-14 01:16:54 +0530

    refactor: move logo to a static import and load it on priority

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,17 +2,20 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { PropsWithChildren } from 'react';
 
+import logo from './logo.svg';
+
 export default function Header({ children }: PropsWithChildren) {
   return (
     <header className="header">
       <div className="flex container justify-between lg:gap-10">
         <Link href="/" className="flex items-center">
           <Image
-            src="/logo.svg"
+            src={logo}
             className="mx-auto md:mx-5 bg-white px-5 rounded-full"
             alt="Estore Logo"
             width={200}
             height={54}
+            priority
           />
         </Link>
         {children}
