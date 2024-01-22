414e0213edec33bdce0e21f9a52eb51b3f14e005
Vinit Sarvade <vinit.sarvade.08@gmail.com>
2024-01-22 18:29:37 +0530

    fix: image decoding

--- src/modules/products/components/product-list-item.tsx
+++ src/modules/products/components/product-list-item.tsx
@@ -32,11 +32,15 @@ export default async function ProductListItem({
   // const { base64, src } = await getImage(product.images.at(0)!.baseUrl);
   const product = productGroup.Products[0];
 
-  const imgUrl = decodeURIComponent(product.ProductImages.at(0)!.image!);
-  const thumbUrl = decodeURIComponent(product.ProductImages.at(0)!.thumbnail!);
+  const imgUrl = product.ProductImages.at(0)!.image!;
+  const thumbUrl = product.ProductImages.at(0)!.thumbnail!;
 
-  const imageSrc = imgUrl.startsWith('http') ? imgUrl : `https:${imgUrl}`;
-  const thumbnailSrc = imgUrl.startsWith('http') ? imgUrl : `https:${thumbUrl}`;
+  const imageSrc = decodeURIComponent(
+    imgUrl.startsWith('http') ? imgUrl : `https:${imgUrl}`,
+  );
+  const thumbnailSrc = decodeURIComponent(
+    imgUrl.startsWith('http') ? imgUrl : `https:${thumbUrl}`,
+  );
 
   return (
     <div
